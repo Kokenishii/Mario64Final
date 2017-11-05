c869e5f8d1763e3b193397945905952e5ea15a31
Unknown <ncarbonara235@gmail.com>
2017-11-05 03:00:21 -0500

    Added Stand-In Player, UI Text, & Script to Track Lives, Stars, & Power
    So far the script doesn't change any of the values yet, just sends them to be displayed by the UI text.
    The only UI text as of current is the persistent text for the number of lives and the number of stars.
    The stand-in player is just a stationary block for now.

---
+++ Assets/_Script/PlayerHealthAndPickups.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthAndPickups : MonoBehaviour {
+
+	public int power;
+	public int lives;
+	public int stars;
+
+	public GameObject starCountText;
+	public GameObject livesCountText;
+
+
+	// Use this for initialization
+	void Start () {
+		power = 8;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Updates the onscreen lives counter
+		livesCountText.GetComponent<Text>().text = "Lives x " + lives;
+
+		//Updates the onscreen stars counter
+		starCountText.GetComponent<Text>().text = "Stars x " + stars;
+	}
+}
