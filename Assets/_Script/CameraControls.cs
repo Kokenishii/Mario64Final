293b5f1a9ea352411c88930fc6bd8892908bd64e
Unknown <mmg551@nyu.edu>
2017-11-07 10:23:07 -0500

    fixed camera controls
    they work now at least

--- Assets/_Script/CameraControls.cs
+++ Assets/_Script/CameraControls.cs
@@ -9,63 +9,64 @@ public class CameraControls : MonoBehaviour {
     //make mario unable to move and then you can move the camera around freely?
 	// Use this for initialization
     public GameObject mario;
-    float startDistanceFromMario;
-    float currentDistanceFromMario;
     public GameObject zoomIn;
     public GameObject zoomOut;
     public GameObject leftSide;
     public GameObject rightSide;
     public GameObject defualtZoom;
-    Vector3 startPosition;
-    Vector3 startRotation;
+    int zoomDistance = 0;
+
+
 	void Start () {
-        startDistanceFromMario = gameObject.transform.position.z - mario.transform.position.z;
-        startRotation = new Vector3(0, 0, 0);
 	}
 	
 	// Update is called once per frame
 	void Update () {
         //zoom in on mario
-        Vector3 marioRotation = new Vector3(mario.transform.rotation.x,mario.transform.rotation.y,mario.transform.rotation.z);
-        currentDistanceFromMario = gameObject.transform.position.z - mario.transform.position.z;
-       if (startDistanceFromMario == currentDistanceFromMario)
-       {
+
+
+
             
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
-                
-                transform.SetPositionAndRotation(zoomIn.transform.position, Quaternion.Euler(marioRotation));
+            if (zoomDistance == 0)
+            {
+                transform.SetPositionAndRotation(zoomIn.transform.position, zoomIn.transform.rotation);
+                zoomDistance = 1;
+            }
+            else
+            {
+                transform.SetPositionAndRotation(defualtZoom.transform.position, defualtZoom.transform.rotation);
+                zoomDistance = 0;
+            }
         }
 
         //zoom out from mario
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
-                transform.SetPositionAndRotation(zoomOut.transform.position, Quaternion.Euler(marioRotation));
-        }
-       }
-        if (startDistanceFromMario != currentDistanceFromMario)
-        {
-            if (Input.GetKeyDown(KeyCode.Alpha1))
+            if (zoomDistance == 0)
             {
-                transform.SetPositionAndRotation(defualtZoom.transform.position, Quaternion.Euler(marioRotation));
+                transform.SetPositionAndRotation(zoomOut.transform.position, zoomOut.transform.rotation);
+                zoomDistance = 2;
             }
+                    else
+                    {
+                        transform.SetPositionAndRotation(defualtZoom.transform.position, defualtZoom.transform.rotation);
+                        zoomDistance = 0;
+                    }
 
-            //zoom out from mario
-            if (Input.GetKeyDown(KeyCode.Alpha2))
-            {
-                transform.SetPositionAndRotation(defualtZoom.transform.position, Quaternion.Euler(marioRotation));
-            }
-        }
+       }
+
          //roate to the left along mario 90 degrees
             if(Input.GetKeyDown(KeyCode.Alpha3))
             {
-                transform.SetPositionAndRotation(leftSide.transform.position, Quaternion.Euler(0,-90,0));
+            transform.SetPositionAndRotation(leftSide.transform.position, leftSide.transform.rotation);
             }
 
         //rotate to the right along mario 90 degrees
             if(Input.GetKeyDown(KeyCode.Alpha4))
             {
-                transform.SetPositionAndRotation(rightSide.transform.position, Quaternion.Euler(0,90,0));
+            transform.SetPositionAndRotation(rightSide.transform.position, rightSide.transform.rotation);
             }
 
 	}
