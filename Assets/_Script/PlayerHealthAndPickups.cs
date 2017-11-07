7486895a9fcb91ecfa5bd047c5d359207c095b53
Unknown <ncarbonara235@gmail.com>
2017-11-07 00:30:18 -0500

    Added Red Coins, Power Meter, Scene Reload on Death, & Game Over Message
    Red coins exist in the sample scene, and will add to a red coin count (which is shown in the debug line for now) as well as the overall "coin score" when collected.
    So far, the power meter is just UI text. Like in the original, it disappears when the player is at full power.
    When the player dies, the entire scene reloads except for the player's number of lives, which carries over to the next reload over.
    When the player goes below zero lives, a debug message plays that tells them they've hit a game over.

--- Assets/_Script/PlayerHealthAndPickups.cs
+++ Assets/_Script/PlayerHealthAndPickups.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+//Lets us talk to scene management
+using UnityEngine.SceneManagement;
+
 public class PlayerHealthAndPickups : MonoBehaviour {
 
 	public int power;
-	public int lives;
+	public static int lives = 3;
 	public int stars;
+	public int coinScore;
+	public int redCoinCount;
 
 	public GameObject starCountText;
+	public GameObject coinCountText;
 	public GameObject livesCountText;
+	public GameObject powerMeter;
 
 	//Creates a Singleton, something that other pieces of code can reference 
 	//to change values in this script
@@ -31,14 +38,35 @@ public class PlayerHealthAndPickups : MonoBehaviour {
 		//Updates the onscreen lives counter
 		livesCountText.GetComponent<Text>().text = "Lives x " + lives;
 
+		//Updates the coin score counter
+		coinCountText.GetComponent<Text>().text = "Coins x " + coinScore;
+
 		//Updates the onscreen stars counter
 		starCountText.GetComponent<Text>().text = "Stars x " + stars;
 
-		//Checks to see if the player has died
-		if (power == 0) {
-			Debug.Log ("You died!");
+		//Checks to see if the player's power is less than maximum and
+		//if the power meter should appear onscreen
+		if (power < 8) {
+			powerMeter.SetActive (true);
+			powerMeter.GetComponent<Text>().text = "Power: " + power;
+		} else {
+			powerMeter.SetActive (false);
 		}
 
+		//Checks to see if the player has died
+		if (power == 0) {
+			lives--;
+			power = 8;
+			Debug.Log ("You died!");
+
+			//Reloads the level (the current number of lives will carry over)
+			SceneManager.LoadScene ("Nick's Systems Test Scene");
+		}
+
+		//Checks to see if the player has lost all their lives
+		if (lives < 0) {
+			Debug.Log ("Game over!");
+		}
 	}
 
 	void OnCollisionEnter(Collision col) {
@@ -50,10 +78,12 @@ public class PlayerHealthAndPickups : MonoBehaviour {
 			Debug.Log ("You win!");
 		}
 
-		//Checks to see if the player has collected a coin
+		//Checks to see if the player has collected a regular coin
 		if (col.gameObject.name == "Coin") {
 
-			if (power > 8) {
+			coinScore++;
+
+			if (power < 8) {
 				power++;
 			}
 
@@ -61,5 +91,21 @@ public class PlayerHealthAndPickups : MonoBehaviour {
 
 			GameObject.Destroy (col.gameObject);
 		}
+
+		//Checks to see if the player has collected a red coin
+		if (col.gameObject.name == "Red Coin") {
+
+			coinScore += 2;
+
+			if (power < 8) {
+				power++;	//NOTE: Do red coins add more power than that?
+			}
+
+			redCoinCount++;
+
+			Debug.Log("Red Coins Collected: " + redCoinCount);
+
+			GameObject.Destroy (col.gameObject);
 	}
+}
 }
