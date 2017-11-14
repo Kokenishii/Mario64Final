b447c4b628e47cf04ea1b8f777d747d264ea2cf2
Unknown <ianouellette47@gmail.com>
2017-11-14 01:00:00 -0500

    Put coins in the level, started moving platforms
    Put the gold and red coins in the level, as faithful to the original level as possible considering what parts of the level we have done. All the moving platforms for the parts we have done have been implemented, with animators moving them. Had some issues with the first and last set of moving blocks in the level, the first don't stay where they should and both of them need at least a few to start mid-animation. We also need to change movement so that Mario moves with platforms he's standing on if he doesn't already, some research shows that parenting whatever block he's on at the time might work.

---
+++ Assets/_Script/DamageSourceScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageSourceScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider col) {
+
+		//Makes sure that it is the player that this object has collided with
+		if (col.gameObject.name == "Player") {
+
+			PlayerHealthAndPickups.Instance.power--;
+			Debug.Log ("Power: " + PlayerHealthAndPickups.Instance.power);
+		}
+	}
+}
