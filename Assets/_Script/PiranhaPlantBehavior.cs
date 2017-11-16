110abd0b5b98324b4876fe548bc854112c0dac8d
Unknown <ncarbonara235@gmail.com>
2017-11-16 03:38:04 -0500

    Created New (Broken) Piranha Plant Script & Prefab
    The gameobjects for the Piranha Plant are all assembled and a script guiding the plant's behavior exists. Currently the script can detect if the player is close to the plant. It also attempts to rotate the plant towards the player if they are in range, but currently this isn't working quite right.
    I also added a blue material for the Thwomp on the side.

--- Assets/_Script/PiranhaPlantBehavior.cs
+++ Assets/_Script/PiranhaPlantBehavior.cs
@@ -13,9 +13,28 @@ public class PiranhaPlantBehavior : MonoBehaviour {
 
 	public PiranhaPlantState currentState;
 
+	//The game object for the player
+	GameObject player;
+
+	//The head (and stem) of the Piranha Plant, which points towards the player when the player is detected,
+	//assigned in the inspector
+	public GameObject piranhaPlantHead;
+
+	//The distance from the center of the Piranha Plant game object
+	//that the player needs to be under in order to wake up the Piranha Plant
+	//and get it to attack,
+	//assigned in the inspector
+	public float distanceToWakeUp;
+
+	//The speed the player needs to be moving in order to wake up the Piranha Plant,
+	//assigned in the inspector
+	public float speedToWakeUp;
 
 	// Use this for initialization
 	void Start () {
+
+		//Assigns the player object
+		player = GameObject.FindWithTag ("Player");
 
 		currentState = PiranhaPlantState.sleeping;
 	}
@@ -26,26 +45,38 @@ public class PiranhaPlantBehavior : MonoBehaviour {
 		//The state of the Piranha when it has not detected the player
 		if (currentState == PiranhaPlantState.sleeping) {
 
-			//Do nothing
+			if (Vector3.Distance (player.transform.position, this.transform.position) < distanceToWakeUp) {
+
+				//IN PROGRESS: This code, meant to prevent the plant from responding to the player
+				//if they're moving slowly enough, still needs fixing.
+				//For now, the plant will automatically move into it's attacking state
+				//regardless of player speed
+
+				//if (player.GetComponent<Rigidbody> ().velocity.magnitude > speedToWakeUp) {
+
+				currentState = PiranhaPlantState.attacking;
+			
+				//}
+			}
 		}
 
 		//The state of the Piranha when it has not detected the player
 		if (currentState == PiranhaPlantState.attacking) {
 
-			//Reduces player health by three
-			PlayerHealthAndPickups.Instance.power -= 3;
-		}
-	}
+			//IN PROGRESS: The following code is meant to allow the plant to rotate to face the player.
+			//Currently it rotates, but not as desired.
 
-	//Checks to see if something, potentially the player, has entered the Piranha Plant's
-	//detection zone
-	void OnCollisionEnter(Collision col) {
+			float headRotationX = piranhaPlantHead.transform.rotation.x;
+			float headRotationZ = piranhaPlantHead.transform.rotation.z;
 
-		Debug.Log ("Collide");
+			piranhaPlantHead.transform.rotation = Quaternion.LookRotation (player.transform.position);
 
-		//If the object is a player, the Piranha Plant will attack
-		if (col.gameObject.tag == "Player") {
-			currentState = PiranhaPlantState.attacking;
+			Vector3 headRotation = piranhaPlantHead.transform.rotation.eulerAngles;
+
+			headRotation.x = headRotationX;
+			headRotation.z = headRotationZ;
+
+			piranhaPlantHead.transform.rotation = Quaternion.LookRotation(headRotation);
 		}
 	}
 }
