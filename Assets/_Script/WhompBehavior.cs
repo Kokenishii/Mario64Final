a6b9002c56a75854a38ae32c7db8d60528971e28
Unknown <ncarbonara235@gmail.com>
2017-11-20 23:07:33 -0500

    Added Basic Movement to Whomps
    Whomps now move back and forth between two points

--- Assets/_Script/WhompBehavior.cs
+++ Assets/_Script/WhompBehavior.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//A script that attaches to the Whomp prefab
+//A script that attaches to the Whomp prefab in order to make it do the 
+//things it does in it's various states
 public class WhompBehavior : MonoBehaviour {
 
+	//The character controller used to regulate the Whomp's movement
+	CharacterController whompCharacterController;
+
 	public enum WhompState {
-		waiting,	//0
+		walking,	//0
 		chasing,	//1
 		falling,	//2
 		fell,	//3
@@ -16,17 +20,69 @@ public class WhompBehavior : MonoBehaviour {
 
 	public WhompState currentState;
 
+	//The locations at which the Whomp turns around and walks
+	//in the other direction
+	public Transform boundaryA;
+	public Transform boundaryB;
 
+	//The speed at which the Whomp walks when it isn't attacking
+	public float walkSpeed;
+
+	//The speed at which the Whomp turns around when it has reached the end of it's movement
+	public float turnSpeed;
+
+	//Determines if the Whomp has turned around at the end of it's rotation
+	bool turnedAround;
+
+	Vector3 walkDirection;
 
 	// Use this for initialization
 	void Start () {
 		
+		whompCharacterController = this.GetComponent<CharacterController> ();
+
+		turnedAround = false;
+
+		walkDirection = Vector3.forward;
+
+		this.transform.rotation = Quaternion.LookRotation (boundaryA.transform.position - transform.position, Vector3.up);
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		if (currentState == WhompState.waiting) {
+		if (currentState == WhompState.walking) {
+
+			if (this.transform.position.z >= boundaryA.position.z
+				&& turnedAround == false) {
+
+				this.transform.rotation = Quaternion.LookRotation (boundaryB.transform.position - transform.position, Vector3.up);
+
+				walkDirection = walkDirection * -1f;
+				turnedAround = true;
+
+			} else if (this.transform.position.z <= boundaryB.position.z
+				&& turnedAround == false) {
+
+				this.transform.rotation = Quaternion.LookRotation (boundaryA.transform.position - transform.position, Vector3.up);
+
+				walkDirection = walkDirection * -1f;
+				turnedAround = true;
+
+			} else {
+
+				whompCharacterController.Move (walkDirection * walkSpeed * Time.deltaTime);
+
+				turnedAround = false;
+
+			}
+
+			//else if whomp is at point B, turn around
+
+			//else, walk forward
+				//if just cleared point A, go one way
+				//if just cleared point B, go the other way
 		}
 
 		if (currentState == WhompState.chasing) {
